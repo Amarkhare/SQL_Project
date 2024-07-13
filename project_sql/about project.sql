66f00d8f6496dc6f16721b484447df7d44472ae7
Amar Khare <amarkhare20@gmail.com>
2024-07-13 20:38:34 +0530

    upload sql files

---
+++ project_sql/about project.sql
@@ -0,0 +1,15 @@
+/*ABOUT_THE_PROJECT
+GOAL - 
+1. You are an aspiring data nerd looking to analyze the top-paying roles and skills
+2. You will create SQL to explore this large dataset specific to you
+3. For those job searching or looking for a promotioin;
+    you can not only use this project to showcase experience 
+    but also to extract what roles/skills you should target
+QUESTIONS TO ANSWER 
+1. What are the top paying jobs for my role ?
+2. What are the skills required for these top_paying roles?
+3. What are the most in demand skills for my role ?
+4. what are the top skills based on salary for my role ?
+5. What are the most optimal skills to learn ?
+    optima - high demand and high paying
+*/
